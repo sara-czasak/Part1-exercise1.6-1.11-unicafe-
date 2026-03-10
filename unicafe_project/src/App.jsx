ecc8202ad8f98afbb17fbe87b9d32408d28d0fde
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-10 19:34:46 +0100

    course exercise 1.7

--- unicafe_project/src/App.jsx
+++ unicafe_project/src/App.jsx
@@ -1,4 +1,14 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
+
+const Average = ({points, totalReviews}) => {
+  const averageReviews= points / totalReviews
+  return <p>Our avridge: {averageReviews}</p>
+}
+
+const PositivePercent = ({positiveReviews, totalReviews}) => {
+  const percent = (positiveReviews / totalReviews) * 100
+  return <p>Positive reviews: {percent}%</p>
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,19 +16,37 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [points, setPoints] = useState(0)
+
+  const [totalReviews, setTotalReviews] = useState(0)
+
+  const [positiveReviews, setPositiveReviews] = useState(0)
+
   const handleGoodClick = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
+    const updatedPoints = points + 1
+    setPoints(updatedPoints)
+    const newTotalReviews = totalReviews + 1
+    setTotalReviews(newTotalReviews)
+    const newPositiveReviews = positiveReviews + 1
+    setPositiveReviews(newPositiveReviews)
   }
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+    const newTotalReviews = totalReviews + 1
+    setTotalReviews(newTotalReviews)
   }
 
   const handleBadClick = () => {
     const updatedBad = bad + 1
     setBad(updatedBad)
+    const updatedPoints = points - 1
+    setPoints(updatedPoints)
+    const newTotalReviews = totalReviews + 1
+    setTotalReviews(newTotalReviews)
   }
 
   return (
@@ -33,6 +61,9 @@ const App = () => {
         <li>NEUTRAL: {neutral}</li>
         <li>BAD: {bad}</li>
       </ul>
+      <p>All reviews: {totalReviews}</p>
+      <Average points={points} totalReviews={totalReviews}></Average>
+      <PositivePercent positiveReviews={positiveReviews} totalReviews={totalReviews}></PositivePercent>
     </div>
   )
 }
